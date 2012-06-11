d766289b0bef737da607b9cd4453753ecf6502d5
Samundra Shrestha <samundra.shr@gmail.com>
2012-06-11 13:32:40 +0545

    Added the Code Documentation

--- web_service.php
+++ web_service.php
@@ -4,23 +4,30 @@
 
 $request = $_SERVER['REQUEST_METHOD'];
 
+// Do not allo the GET request
 if($request=="GET") {
   header('status : 405 Method not Allowed');
   print 'Method Not Allowed';
   exit;
 }
 
+// Write to log file in case if we have any problem
 $fp = fopen('log.txt','w');
 fwrite($fp, serialize($_SERVER['POST']['username']));
 fclose($fp);
 
+// Get the actual POST request
 $username = $_POST['username'];
 $password = $_POST['password'];
 
+// Our Simple Logic implementation 
 if($username=='ritesh' && $password=='pass'){
+  
+  // set Header Tpe to plain/text
   header('content-type:plain/text');
   print "OK";
 }else {
+  // set Header Tpe to plain/text
   header('content-type:plain/text');
   print "CANCEL";
 }
